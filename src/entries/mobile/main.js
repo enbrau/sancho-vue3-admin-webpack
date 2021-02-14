7552d5e74661a0d4ed465bafad53f5ed4b00ac46
Enbrau <admin@enbrau.com>
2021-02-14 21:24:21 +0800

    feat 移动端入口

---
+++ src/entries/mobile/main.js
@@ -0,0 +1,31 @@
+import { init } from '@/utils/framework'
+// Settings
+import settings from './settings'
+// Global Variable Jobs
+import { day, lodash, dict } from '@/utils/globals'
+// Main Component
+import App from './App.vue'
+// I18N
+import i18n from '@/i18n'
+const locale = navigator.language || navigator.userLanguage || 'zh-CN'
+// Icons
+import icons from '@/icons'
+// Router
+import router from '@/router/mobile'
+// Vuex
+import store from '@/store'
+// UI: ElementPlus
+import Vant from 'vant'
+import 'vant/lib/index.css'
+// UI: Custom Styles
+// import '@/styles/index.scss'
+
+init(settings, [day, lodash, dict], App, app => {
+  app
+    .use(i18n, locale)
+    .use(icons)
+    .use(router)
+    .use(store)
+    .use(Vant)
+    .mount('body')
+})
---
+++ src/entries/mobile/settings.js
@@ -0,0 +1,8 @@
+import lodash from 'lodash'
+import settings from '@/settings'
+
+export default lodash.extend(settings, {
+
+  title: '手机'
+
+})
