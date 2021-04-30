163f08f59e793d2fc252015edbe0787066b15380
enbrau <admin@enbrau.com>
2021-04-30 17:31:55 +0800

    fix 修复路由keep-alive问题

--- src/entries/main/App.vue
+++ src/entries/main/App.vue
@@ -1,7 +1,7 @@
 <template>
   <router-view v-slot="{ Component }">
     <keep-alive>
-      <component :is="Component" />
+      <component :is="Component" :key="$route.path" />
     </keep-alive>
   </router-view>
 </template>
